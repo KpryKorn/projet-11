053cb9e12025715315f32c6466525848b1580b65
KpryKorn <roffinisacha@gmail.com>
2024-10-31 17:44:10 +0100

    feat: details component

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -10,8 +10,8 @@ export default function Details({
   children?: React.ReactNode;
 }) {
   return (
-    <details className="flex-1 bg-red-kasa">
-      <summary>{text}</summary>
+    <details className="flex-1 bg-red-kasa p-2 rounded-lg text-white">
+      <summary className="font-medium">{text}</summary>
       {children}
     </details>
   );
