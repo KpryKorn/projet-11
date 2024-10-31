648af729af037ed0f8ae558424b8481afdbcec20
KpryKorn <roffinisacha@gmail.com>
2024-10-31 15:03:21 +0100

    feat: handles not found logement id

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,7 @@ import data from "../db/data.json";
 export const getLogements = () => {
   return data;
 };
+
+export const getLogementId = (id: string) => {
+  return data.find((logement) => logement.id === id);
+};
--- src/pages/logement.tsx
+++ src/pages/logement.tsx
@@ -1,7 +1,13 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
+import { getLogementId } from "../lib/utils";
 
 export default function Logement() {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
+  const logement = getLogementId(id!);
+
+  if (!logement) {
+    return <Navigate to={"*"} replace />;
+  }
 
   return <h1>Logement portant l'id {id}</h1>;
 }
