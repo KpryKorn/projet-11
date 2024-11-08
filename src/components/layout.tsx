f07c49cf4617eb118b2b9537b341c346b02b4577
KpryKorn <roffinisacha@gmail.com>
2024-11-08 09:25:23 +0100

    fix: remove "min-h-screen" layout styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <>
       <Header />
-      <main className="min-h-screen container mx-auto">
+      <main className="container mx-auto">
         <Outlet />
       </main>
       <Footer />
--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -4,8 +4,13 @@ export default function NotFound() {
   return (
     <div className="text-red-kasa text-center flex flex-col gap-[139px] items-center justify-center">
       <h1 className="font-bold text-[288px]">404</h1>
-      <p className="font-medium text-4xl">Oups! La page que vous demandez n'existe pas.</p>
-      <Link to={"/"} className="text-black text-lg underline underline-offset-4">
+      <p className="font-medium text-4xl">
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+      <Link
+        to={"/"}
+        className="text-black text-lg underline underline-offset-4 mb-8"
+      >
         Retourner sur la page d'accueil
       </Link>
     </div>
